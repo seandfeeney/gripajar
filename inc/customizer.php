ded6bfe2faa1ebe1b55e0ea9749c776eec91be23
seandfeeney <seandfeeney@gmail.com>
2017-10-01 22:21:46 -0400

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * GRIPAJAR Theme Customizer
+ *
+ * @package GRIPAJAR
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function gripajar_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'gripajar_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function gripajar_customize_preview_js() {
+	wp_enqueue_script( 'gripajar_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'gripajar_customize_preview_js' );
